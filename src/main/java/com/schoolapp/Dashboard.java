52aa3fe0e07e8ae1cffa9f0da434b1a3fef6460c
dud3k88 <mateusz.dudek88@gmail.com>
2021-02-06 18:31:34 +0100

    06.02_04

--- src/main/java/com/schoolapp/Dashboard.java
+++ src/main/java/com/schoolapp/Dashboard.java
@@ -20,6 +20,7 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "", layout = Menu.class)
